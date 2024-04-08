8868aff62b7ef424fc34717284c7690b9c741a80
Fab1o0107 <fabio0107idcraft@gmail.com>
2024-04-08 23:42:19 +0700

    refactor(extractImage): use common imports

--- extractImage.mjs
+++ extractImage.mjs
@@ -3,83 +3,33 @@
 import { readFileSync } from "node:fs";
 import { resolve } from "node:path";
 import { EOL } from "node:os";
-import meow from "meow";
 
-const cli = meow(
-	`
-    Usage
-      $ node ./extractImage.mjs [options] <Dockerfile path> [key]
+const path = process.argv.at(2);
+const key = process.argv.at(3);
 
-    Options
-      --apply-variant, -a    Toggle whether to apply variant to tags or not. Type: boolean. Default: true
-
-      --seperator, -s        What seperator to use for Array if [key] query is used. use "\\n" for newline. Type: string. Default: ", "
-
-      --reverse-tags, -r     Reverse the tags array. Type: boolean. Default: false
-
-      --help                 Print help menu
-`, {
-		importMeta: import.meta,
-		flags: {
-			applyVariant: {
-				type: "boolean",
-				shortFlag: "a",
-				default: true,
-			},
-			seperator: {
-				type: "string",
-				shortFlag: "s",
-				default: ", ",
-			},
-			reverseTags: {
-				type: "boolean",
-				shortFlag: "r",
-				default: false,
-			},
-		},
-	},
-);
-
-const path = cli.input.at(0);
-const key = cli.input.at(1);
-const { applyVariant: variantApply, reverseTags, seperator } = cli.flags;
-
-/** @param {string} string */
-const getValue = string => /[^:]+$/.exec(string)?.at(0).trim();
-/**
- * @param {string} string
- * @param {string} variant
- */
+const getValue = string => /[^:]+$/u.exec(string)?.at(0).trim();
 const applyVariant = (string, variant) => `${string}-${variant}`;
 
-if (path) {
-	const lines = readFileSync(resolve(path)).toString().split(EOL);
+const lines = readFileSync(resolve(path)).toString().split(EOL);
+const originalTags = getValue(lines.at(0))?.split(", ") ?? [];
+const variant = getValue(lines.at(1));
 
-	const originalTags = getValue(lines.at(0))?.split(", ") ?? [];
-	const variant = getValue(lines.at(1));
-	const platforms = getValue(lines.at(2))?.split(", ") ?? [];
-	const version = getValue(lines.at(3)).split("-").at(0);
+const version = getValue(lines.at(3)).split("-").at(0);
+const semverArray = version.split(".");
+const versions = semverArray.map((_, i) => semverArray.slice(0, semverArray.length - i).join("."));
 
-	const semverArray = version.split(".");
-	const versions = semverArray.map((_, i) => semverArray.slice(0, semverArray.length - i).join("."));
+const recipe = {
+    tags: [...versions, ...originalTags].map(t => (applyVariant(t, variant))),
+    variant,
+    platforms: getValue(lines.at(2))?.split(", ") ?? [],
+    version
+};
 
-	const tags = [...versions, ...originalTags].map(t => (variantApply && variant ? applyVariant(t, variant) : t));
+if (key) {
+	let value = recipe[key];
+	if (value instanceof Array) value = value.join("\n");
 
-	const recipe = {
-		tags: reverseTags ? tags.reverse() : tags,
-		variant,
-		platforms,
-		version
-	};
-
-	if (key) {
-		let value = recipe[key];
-		if (value instanceof Array) value = value.join(seperator === "\\n" ? "\n" : seperator);
-
-		console.info(value);
-	} else {
-		console.info(JSON.stringify(recipe, null, 4));
-	}
+	console.info(value);
 } else {
-	cli.showHelp(1);
+	console.info(JSON.stringify(recipe, null, 4));
 }
